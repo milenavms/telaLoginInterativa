d8516cfdaab1d47fb4cb3ab4d30dbe9bde44e59d
milenavms <milenavasconcelos.info@gmail.com>
2020-06-18 13:58:01 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var btnSignin = document.querySelector("#signin");
+
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+btnSignup.addEventListener("click", function(){
+    body.className = "sign-up-js"; 
+}
+);
+ 
+btnSignin.addEventListener("click", function(){
+    body.className = "sign-in-js"; 
+}
+);
+
+
